627fae6f751bd11580d7ec1ce1dedd9a852ef0f7
cornprincess <xiaocorn96@gmail.com>
2020-03-19 23:54:54 +0800

    TODO

---
+++ algorithms/java/LongestPalindrome/LongestPalindrome.java
@@ -0,0 +1,32 @@
+// Source : https://leetcode.com/problems/longest-palindrome/
+// Author : cornprincess
+// Date   : 2020-03-19
+
+/***************************************************************************************************** 
+ *
+ * Given a string which consists of lowercase or uppercase letters, find the length of the longest 
+ * palindromes that can be built with those letters.
+ * 
+ * This is case sensitive, for example "Aa" is not considered a palindrome here.
+ * 
+ * Note:
+ * Assume the length of given string will not exceed 1,010.
+ * 
+ * Example: 
+ * 
+ * Input:
+ * "abccccdd"
+ * 
+ * Output:
+ * 7
+ * 
+ * Explanation:
+ * One longest palindrome that can be built is "dccaccd", whose length is 7.
+ * 
+ ******************************************************************************************************/
+
+package LongestPalindrome;
+
+public class LongestPalindrome {
+    // TODO
+}
